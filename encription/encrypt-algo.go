6dfad4a7ce03419c725aa909a012c7596e8d985c
Martande8055 <smartande8055@gmail.com>
2023-04-27 22:59:32 +0530

    addend commits

--- encription/encrypt-algo.go
+++ encription/encrypt-algo.go
@@ -1,5 +1,8 @@
 package encription
 
+// an identifier starts with uppercase letter is exported.
+// an identifier starts with lowercase letter or underscore can
+// only accessed from within package where it's declared.
 func Encrypt(str string) string {
 
 	var enc_str string = ""
